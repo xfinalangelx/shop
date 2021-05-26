4a7dc4c3cc5ecfac792bab5b034d0ee87c05e11b
finalangel <55591218+xfinalangelx@users.noreply.github.com>
2021-05-26 22:16:49 +0800

    migrate db

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
-define("DB_HOST", "sql6.freemysqlhosting.net");
-define("DB_USER", "sql6414995");
-define("DB_PASS", "apJq2rn3zx");
-define("DB_NAME", "sql6414995");
+define("DB_HOST", "mysql5.us.freehostia.com");
+define("DB_USER", "johlee84");
+define("DB_PASS", "dangomartum2021");
+define("DB_NAME", "johlee84_db_shop");
 
 
