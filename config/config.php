a2c6a0cb0c659f671b87c2dc4e434a3279d6851b
finalangel <55591218+xfinalangelx@users.noreply.github.com>
2021-06-06 19:52:34 +0800

    Change db

--- config/config.php
+++ config/config.php
@@ -1,7 +1,5 @@
 <?php
 define("DB_HOST", "sql6.freemysqlhosting.net");
-define("DB_USER", "sql6414995");
-define("DB_PASS", "apJq2rn3zx");
-define("DB_NAME", "sql6414995");
-
-
+define("DB_USER", "sql6417445");
+define("DB_PASS", "sDVK9UETcK");
+define("DB_NAME", "sql6417445");
